4248cbfe7a35a2382660b03aef4109ce705dd12c
Kamil Michalski <kmichalski@outlook.com>
2024-01-27 15:34:02 +0100

    e2e advanced

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,15 +1,37 @@
+const baseUrl = 'http://localhost:5173/';
+
 describe('home page', () => {
   it('should be default set to polish language', () => {
-    cy.visit('http://localhost:5173/');
+    cy.visit(baseUrl);
 
     cy.get('h1').contains('Pracownicy');
   })
 
   it('should change language to english', () => {
-    cy.visit('http://localhost:5173/')
+    cy.visit(baseUrl)
 
     cy.get('[data-testid="language-selector"]').select("en");
 
     cy.get('h1').contains('Employees');
   })
+
+  it('should add employee', () => {
+    cy.visit(baseUrl);
+
+    cy.getByTestId('add-button').click();
+
+    cy.get('#firstname').type('Kamil');
+    cy.get('#lastname').type('Nowak');
+    cy.get('#birthdate').type('1.1.2000');
+    cy.get('#address').type('Warszawska 20');
+    cy.get('#city').type('Katowice');
+    cy.get('#postalcode').type('40-000');
+    cy.get('#phonenumber').type('444555666');
+    cy.get('#salary').type('10000');
+
+    cy.get('[type="submit"]').click();
+
+    cy.contains('Kamil');
+    cy.contains('Nowak');
+  });
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,3 +35,7 @@
 //     }
 //   }
 // }
+
+Cypress.Commands.add('getByTestId', (selector, ...args) => {
+    return cy.get(`[data-testid="${selector}"]`, ...args);
+})
